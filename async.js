5962204cfa703ecf1a35bac944f8312168b82700
dru <denginru@yandex.ru>
2017-11-30 16:46:02 +0500

    first attempt

--- async.js
+++ async.js
@@ -9,5 +9,48 @@ exports.runParallel = runParallel;
  * @param {Number} timeout - таймаут работы промиса
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    var results = [];
+
+    return new Promise(resolve => {
+        if (jobs.length === 0 || parallelNum <= 0) {
+            resolve(results);
+
+            return;
+        }
+
+        var jobCounter = 0;
+
+        var anotherPartOfJobs = jobs.slice(0, parallelNum);
+        for (var i = 0; i < anotherPartOfJobs.length; i++) {
+            exec(anotherPartOfJobs[i], i);
+            jobCounter++;
+        }
+
+        function exec(job, index) {
+            var proc = res => proceed(res, index);
+            Promise.race(
+                [
+                    job(),
+                    new Promise(rejected => setTimeout(rejected, timeout, new Error('timeouted')))
+                ]
+            )
+                .then(proc)
+                .catch(proc);
+        }
+
+        function proceed(res, index) {
+            results[index] = res;
+            if (results.length === jobs.length) {
+                resolve(results);
+
+                return;
+            }
+
+            if (jobCounter < jobs.length) {
+                var jobIndex = jobCounter;
+                jobCounter++;
+                exec(jobs[jobIndex], jobCounter);
+            }
+        }
+    });
 }
